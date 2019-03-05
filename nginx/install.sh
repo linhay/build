9464b1cce003f94eb6971e6a2cbde326bf2e440b
linhay <158179948@qq.com>
2019-03-05 15:09:09 +0800

    update

--- nginx/install.sh
+++ nginx/install.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+
+apt-get update
 apt-get install nginx -y
-cp -fr ./backup/* ~/nginx/
-cp -fr ./base/* /etc/nginx/
+
+if [ ! -d "~/nginx" ]; then
+  mkdir ~/nginx
+  cp -fr ./backup/* ~/nginx/
+  cp -fr ./base/* /etc/nginx/
+else 
+  echo "~/nginx 已存在, 请查看或删除"
+fi
+
+
